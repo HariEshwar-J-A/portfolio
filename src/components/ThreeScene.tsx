8c03e864152d6d87ed5df1042a805c5ff113df10
Harieshwar J A <36564717+HariEshwar-J-A@users.noreply.github.com>
2025-05-23 05:47:06 -0400

    Add missing useThree import

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState, useMemo } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { 
   Sphere, 
   Stars, 
