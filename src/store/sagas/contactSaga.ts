2529b86b3162164ed838e552221c7b50e6619f10
Harieshwar J A <36564717+HariEshwar-J-A@users.noreply.github.com>
2025-05-26 13:24:18 -0400

    Fix contact saga implementation

--- src/store/sagas/contactSaga.ts
+++ src/store/sagas/contactSaga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, call, put, select, fork } from 'redux-saga/effects';
+import { takeLatest, call, put, select } from 'redux-saga/effects';
 import { submitForm, submitSuccess, submitFailure } from '../slices/contactSlice';
 import emailjs from '@emailjs/browser';
 import { portfolioData } from '../../data/portfolioData';
@@ -10,27 +10,24 @@ interface EmailParams {
   message: string;
 }
 
-function* sendEmail() {
-  const params = yield select((state) => state.contact.form);
-  console.log(params);
-  const templateParams = {
-    name: params.name,
-    email: params.email,
-    subject: params.subject,
-    message: params.message
-  };
-
-  return emailjs.send(
-    import.meta.env.VITE_EMAILJS_SERVICE_ID,
-    import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-    templateParams
-  );
-}
-
 function* handleSubmitForm(action: ReturnType<typeof submitForm>) {
   try {
-    // const localState = yield select((state: any) => state.contact.form);
-    yield fork(sendEmail);
+    const formData: EmailParams = yield select((state: any) => state.contact.form);
+    
+    const templateParams = {
+      to_name: 'Admin',
+      from_name: formData.name,
+      from_email: formData.email,
+      subject: formData.subject,
+      message: formData.message,
+    };
+
+    yield call(emailjs.send,
+      import.meta.env.VITE_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      templateParams
+    );
+    
     yield put(submitSuccess());
   } catch (error) {
     yield put(submitFailure(error instanceof Error ? error.message : 'An error occurred'));
@@ -39,9 +36,4 @@ function* handleSubmitForm(action: ReturnType<typeof submitForm>) {
 
 export function* watchContact() {
   yield takeLatest(submitForm.type, handleSubmitForm);
-}
-
-// Add this to fix the TypeScript error
-function select(selector: any) {
-  return { type: 'SELECT', selector };
 }
